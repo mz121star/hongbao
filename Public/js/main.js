f0ff58a22b2826bb107e8a1905c977a4dd782f80
mz121star <mz121star@gmail.com>
2015-01-05 15:10:33 +0800

    优化红包

--- Public/js/main.js
+++ Public/js/main.js
@@ -3,6 +3,6 @@ $(function(){
     var w=($(window).width()-$("#mcover .zhongjiangtip").width())/2 ;
     $("#mcover .zhongjiangtip").css({"left":w}).show() ;
     $(".main-box").css("top",($(window).height()-478)/3+"px")   ;
-    $("#shape").css("top",($(window).height()-478)/3+420+"px")    ;
+    ;
 
 })
