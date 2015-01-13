a123264afbd11d5f9531a80a5e8dac1a9ca48cb6
zhaobin <zhaobin@deepin.deepin-domain>
2015-01-13 15:30:27 +0800

    add

---
+++ Application/Home/Controller/VoteController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Home\Controller;
+
+class VoteController extends BaseController {
+
+    private $app_id = 'wxc43356a7940e32d4';
+
+    private $app_secret = 'ec234926610a429dfaca36328af9b014';
+
+    public function indexAction() {
+        $this->display();
+    }
+}
