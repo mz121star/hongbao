77e4d357113525431603f3588fb1f74319950c38
mz121star <mz121star@gmail.com>
2015-03-12 22:13:24 +0800

    优化红包

---
+++ Application/Common/Common/pay.php
@@ -0,0 +1,54 @@
+<?php
+class Packet{
+
+	private $wxapi;
+    function _route($fun,$param = ''){
+ 
+		@require_once "oauth2.php";
+		$this->wxapi = new Wxapi();
+		switch ($fun)
+		{
+			case 'userinfo':
+				return $this->userinfo($param);
+				break;
+			case 'wxpacket':
+				return $this->wxpacket($param);
+				break;			
+			default:
+				exit("Error_fun");
+		}		
+    }	
+    /**
+     * 用户信息
+     * 
+     */	
+	private function userinfo($param){ 
+	
+		$get = $param['param'];
+		$code = $param['code'];	
+		$parentid=$param['parentid'];	
+		if($get=='access_token' && !empty($code)){	 
+			$json = $this->wxapi->get_access_token($code);
+			if(!empty($json)){
+				$userinfo = $this->wxapi->get_user_info($json['access_token'],$json['openid']);
+ 
+				return $userinfo;
+			}
+		}else{
+			  
+				$return_url="http://weishangcheng.webs.dlwebs.com/hongbao/index.php?param=access_token&parentid=".$parentid;
+				$this->wxapi->get_authorize_url($return_url,'STATE');
+			 
+ 
+			
+		}
+	}
+    /**
+     * 微信红包
+     * 
+     */		
+	private function wxpacket($param){
+		return $this->wxapi->pay($param['openid']);
+	}
+}
+?>
